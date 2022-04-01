5275213cb83b8a382fc7b7aaecf5ca5adf4b793e
teklu f-a <teklufa@gmail.com>
2022-04-01 10:45:23 +0300

    vote anecdote, show most voted anecdote

--- README.md
+++ README.md
@@ -2,9 +2,16 @@
 
 Exercises 
 
-## Exercises 1.6 - 1.14
+## Exercises 1.6 - 1.11
 
 Unicafe feedback
 
 an app to collect customer feedback
 
+## Exercise 1.12 - 1.14
+
+Anecdote
+
+shows a random anecdote from the field of software engineering,
+and allow users to vote from the list of anecdotes
+the most voted anecdote will be shown as well
--- src/components/Anecdote/index.js
+++ src/components/Anecdote/index.js
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import { anecdotes } from '../../data/anecdote.js'
+import { useState, useEffect } from 'react'
+import { anecdotes as anecdotesData } from '../../data/anecdote.js'
 
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
@@ -8,18 +8,36 @@ import Stack from '@mui/material/Stack'
 import Box from '@mui/material/Box'
 
 const Anecdote = (props) => {
+  const [ anecdotes, setAnecdotes ] = useState(anecdotesData)
   const [ selected, setSelected ] = useState(Math.floor(Math.random() * anecdotes.length))
+  const [ mostVoteAnecdote, setMostVoteAnecdote ] = useState({})
 
   const handleNextAnecdote = () => {
     setSelected(Math.floor(Math.random() * anecdotes.length))
   }
   const handleAnecdoteVote = () => {
-
+    const copyAnecdotes = [...anecdotes]
+    copyAnecdotes[selected] = {...copyAnecdotes[selected], vote: copyAnecdotes[selected].vote + 1 }
+    setAnecdotes(copyAnecdotes)
   }
+
+
+  useEffect(() => {
+    const maxVoteAnecdote = () => {
+      const maxVote = Math.max(...anecdotes.map(anecdote => anecdote.vote))
+      return anecdotes.find(anecdote => anecdote.vote === maxVote)
+    }
+    setMostVoteAnecdote(maxVoteAnecdote())
+  }, [anecdotes])
+
   return (
     <Stack spacing={1} sx={{ mx: '20px'}}>
+      <Typography variant='h6' sx={{ fontWeight: 'bold'}}>Anecdote</Typography>
       <Typography>
         { anecdotes[selected].anecdote }
+      </Typography>
+      <Typography>
+        has <Typography component='span' sx={{ fontWeight: 'bold' }}>{ anecdotes[selected].vote }</Typography> vote/s
       </Typography>
       <Box sx={{my: '10px' }}>
         <Button variant='outlined' onClick={handleAnecdoteVote} sx={{ mr: '10px'}}>
@@ -29,8 +47,21 @@ const Anecdote = (props) => {
           Next Anecdote
         </Button>
       </Box>
-    </Stack>
-  );
-};
+      {
+        (mostVoteAnecdote.vote === 0)
+          ? null
+          : (
+              <>
+                <Typography variant='h6' sx={{ fontWeight: 'bold'}}>Anecdote with most vote</Typography>
+                <Box>
+                  <Typography>{mostVoteAnecdote.anecdote}</Typography>
+                  <Typography sx={{ fontWeight: 'bold', my: '10px'}}>with {mostVoteAnecdote.vote} vote</Typography>
+                </Box>
+              </>
+            )
+        }
+      </Stack>
+    );
+  };
 
 export default Anecdote
